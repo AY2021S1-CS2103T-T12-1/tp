c4994e0f2a3ee8b5e8ee4560b88e375ddb802b60
zeranium97 <zeranium97@gmail.com>
2020-09-15 01:01:29 +0800

    update checkstyle

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -10,6 +10,11 @@ public class Remark {
 
     public final String value;
 
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param Remark A remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
